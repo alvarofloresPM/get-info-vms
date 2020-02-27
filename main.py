bfd312c65b2580d12720b419b0cc96cb28110be6
alvarofloresPM <alvaro.flores@processmaker.com>
2020-02-27 12:37:22 -0400

    testt

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def windowsinfo():
     Huser = os.getenv('HVuser')
     Hpass = os.getenv('HVpass')
     PM_ip = '192.168.100.201'
-    s = winrm.Session(PM_ip, auth=(Huser, Hpass))
+    s = winrm.Session(str(PM_ip), auth=(Huser, Hpass))
     ht = s.run_ps('(Get-VM).count')
     print(ht)
     print("hola esto es un ejemplo")
