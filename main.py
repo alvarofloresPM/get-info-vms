ea3bf7b99e5852d78403060a81d55256d1d05146
alvarofloresPM <alvaro.flores@processmaker.com>
2020-02-27 16:18:40 -0400

    test

--- main.py
+++ main.py
@@ -18,17 +18,18 @@ def windowsinfo(server_ip):
     s = winrm.Session(server_ip, auth=(Huser, Hpass))
     ht = s.run_ps('(Get-VM).count')
     ht = ht.std_out
+    mycursor = mydb.cursor()
     for x in range(int(ht)):
         vm_name = s.run_ps("Get-VM | Select -ExpandProperty Name | Select-Object -Index " + str(x) )
         print (vm_name.std_out) 
-        mycursor = mydb.cursor()
         mycursor.execute("SELECT server_name FROM server WHERE server_name='" + vm_name.std_out + "'" )
         myresult = mycursor.fetchone()
         if myresult == vm_name.std_out:
             print ("Igual --------")
         else:
             print ("NAda -------- ")
-        mydb.close()
+    mycursor.close()
+    mydb.close()
     # vm_count = int(vm_count)-1
     # vm_name = s.run_ps('Get-VM | Select -ExpandProperty Name | Select-Object -Index ('+ vm_count + ')' )
     # print (vm_name)
