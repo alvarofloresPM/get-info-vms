48aba629884f837be80cea24722ae67ba43ba134
alvarofloresPM <alvaro.flores@processmaker.com>
2020-02-27 12:40:14 -0400

    pol

--- main.py
+++ main.py
@@ -22,5 +22,4 @@ def windowsinfo():
     # vm_count = int(vm_count)-1
     # vm_name = s.run_ps('Get-VM | Select -ExpandProperty Name | Select-Object -Index ('+ vm_count + ')' )
     # print (vm_name)
-    return;
 windowsinfo()
