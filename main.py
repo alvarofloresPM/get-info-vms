94f1cc3fc71a592354bc14a38d3d1a7a65d1b1ed
alvarofloresPM <alvaro.flores@processmaker.com>
2020-02-27 15:48:02 -0400

    test

--- main.py
+++ main.py
@@ -22,12 +22,9 @@ def windowsinfo(server_ip):
         vm_name = s.run_ps("Get-VM | Select -ExpandProperty Name | Select-Object -Index " + str(x) )
         print (vm_name.std_out) 
         mycursor = mydb.cursor()
-        if mycursor.execute("SELECT server_name FROM server WHERE server_name='" + vm_name.std_out + "'" ):
-            myresult = mycursor.fetchone()
-            for x in myresult:
-                print(x)
-        else:
-            print ("NO ---------------------------")
+        mycursor.execute("SELECT server_name FROM server WHERE server_name='" + vm_name.std_out + "'" )
+        myresult = mycursor.fetchone()
+        print (myresult)
         mydb.close()
     # vm_count = int(vm_count)-1
     # vm_name = s.run_ps('Get-VM | Select -ExpandProperty Name | Select-Object -Index ('+ vm_count + ')' )
