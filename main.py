904adb7b6fdde7d3b907b3d943013d8a212c1794
alvarofloresPM <alvaro.flores@processmaker.com>
2020-02-28 18:34:58 -0400

    r

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def createnewserver(data, master):
     # server_domain
     nmScan.scan(server_ip, '21-443')
     #response = str(nmScan[server_ip]['hostnames'][0]['name'])
-    response = nmScan[server_ip]['tcp']['22']['state']
+    response = nmScan[server_ip]
     print (type(response))
     print (response)
     if response is not None:
