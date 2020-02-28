7a0d1d4a28fe6ea95f291362df59fa29720537fb
alvarofloresPM <alvaro.flores@processmaker.com>
2020-02-28 09:11:38 -0400

    test

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def windowsinfo(server_ip):
         myresult = mycursor.fetchone()
         myresult = str(myresult[0])
         vm_names = vm_name.std_out
-        vm_names = vm_names.rstrip('\n')
+        vm_names = vm_names.rstrip()
         print (myresult)
         print (vm_names)
         if ( myresult == vm_names ):
