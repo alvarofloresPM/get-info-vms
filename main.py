5f0ec13449c38639d7919577da6aadefd8ea1dca
alvarofloresPM <alvaro.flores@processmaker.com>
2020-03-04 10:45:01 -0400

    test

--- main.py
+++ main.py
@@ -291,8 +291,8 @@ def windowsinfomaster(master_ip, master_name):
     master_server_s = str(hs)
     
     mycursor = mydb.cursor()
-    sql = "INSERT INTO master (master_ip, master_name, master_ram, master_space, master_servers_t, master_servers_r, master_servers_o, master_servers_s) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-    val = (master_ip, master_name, master_ram, master_space, master_server_t, master_server_r, master_server_o, master_server_s)
+    sql = "UPDATE server SET master_ram = %s, master_space = %s, master_servers_t = %s, master_servers_r = %s, master_servers_o = %s, master_servers_s = %s WHERE master_ip = %s"
+    val = (master_ram, master_space, master_server_t, master_server_r, master_server_o, master_server_s, master_ip)
     mycursor.execute(sql, val)
     mydb.commit()
     print(mycursor.rowcount, "record inserted.")
