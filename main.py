e7bb3a98ec23ce7e891437457ece1cef11a111cd
alvarofloresPM <alvaro.flores@processmaker.com>
2020-03-06 18:49:15 -0400

    test

--- main.py
+++ main.py
@@ -259,7 +259,8 @@ def windowsinfo(master_ip, master_name):
     ht = s.run_ps('(Get-VM).count')
     ht = ht.std_out
     mycursor = mydb.cursor()
-    for x in range(int(ht)):
+    # for x in range(int(ht)):
+    for x in range(int(5)):
         vm_name = s.run_ps("Get-VM | Select -ExpandProperty Name | Select-Object -Index " + str(x) )
         vm_names = vm_name.std_out
         vm_names = vm_names.rstrip()
