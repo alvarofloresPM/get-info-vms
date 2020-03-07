1ac9534484c0590b8ca53b47f9312f7fb54f9047
alvarofloresPM <alvaro.flores@processmaker.com>
2020-03-07 19:41:39 -0400

    release

--- main.py
+++ main.py
@@ -261,8 +261,8 @@ def windowsinfo(master_ip, master_name):
     ht = s.run_ps('(Get-VM).count')
     ht = ht.std_out
     mycursor = mydb.cursor()
-    #for x in range(int(ht)):
-    for x in range(int(10)):
+    for x in range(int(ht)):
+    #for x in range(int(10)):
         vm_name = s.run_ps("Get-VM | Select -ExpandProperty Name | Select-Object -Index " + str(x) )
         vm_names = vm_name.std_out
         vm_names = vm_names.rstrip()
@@ -371,21 +371,21 @@ def windowsinfomaster(master_ip, master_name):
 
 # MAIN section
 windowsinfo("192.168.100.200","HYPNOS")
-# windowsinfo("192.168.100.201","THANATOS")
-# windowsinfo("192.168.100.202","ULTRAMAGNUS")
-# windowsinfo("192.168.100.205","PHOBOS")
-# windowsinfo("192.168.100.206","OPTIMUS")
+windowsinfo("192.168.100.201","THANATOS")
+windowsinfo("192.168.100.202","ULTRAMAGNUS")
+windowsinfo("192.168.100.205","PHOBOS")
+windowsinfo("192.168.100.206","OPTIMUS")
 
-# itwasdeleted("192.168.100.200","HYPNOS")
-# itwasdeleted("192.168.100.201","THANATOS")
-# itwasdeleted("192.168.100.202","ULTRAMAGNUS")
-# itwasdeleted("192.168.100.205","PHOBOS")
-# itwasdeleted("192.168.100.206","OPTIMUS")
+itwasdeleted("192.168.100.200","HYPNOS")
+itwasdeleted("192.168.100.201","THANATOS")
+itwasdeleted("192.168.100.202","ULTRAMAGNUS")
+itwasdeleted("192.168.100.205","PHOBOS")
+itwasdeleted("192.168.100.206","OPTIMUS")
 
-# windowsinfomaster("192.168.100.200","HYPNOS")
-# windowsinfomaster("192.168.100.201","THANATOS")
-# windowsinfomaster("192.168.100.202","ULTRAMAGNUS")
-# windowsinfomaster("192.168.100.205","PHOBOS")
-# windowsinfomaster("192.168.100.206","OPTIMUS")
+windowsinfomaster("192.168.100.200","HYPNOS")
+windowsinfomaster("192.168.100.201","THANATOS")
+windowsinfomaster("192.168.100.202","ULTRAMAGNUS")
+windowsinfomaster("192.168.100.205","PHOBOS")
+windowsinfomaster("192.168.100.206","OPTIMUS")
 
 verifyipserver()
