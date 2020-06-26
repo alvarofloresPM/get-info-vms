5c92830d7fc541801db4876229a95a75485173a2
alvarofloresPM <alvaro.flores@processmaker.com>
2020-06-26 14:36:14 -0400

    new filter

--- main.py
+++ main.py
@@ -322,8 +322,11 @@ def windowsinfomaster(master_ip, master_name):
     m = m.rstrip()
     master_ram = str(m)
     d = s.run_ps('Get-WMIObject -Class Win32_LogicalDisk | Where-Object {$_.DriveType -eq 3} | Select-Object @{n="Unidad";e={($_.Name)}}, @{n="Libre (GB)";e={"{0:n0}" -f ($_.freespace/1gb)}} | Format-List').std_out
+    print(d)
     d = d.rstrip()
+    print(d)
     disk_c = re.findall("[CDEZ]{1,9}:\nLibre \(GB\) : ([0-9.,]{1,9})", d)
+    print(str(disk_c[0]))
     if len(disk_c[0]) != 0:
         disk_cr = str(disk_c[0])
         disk_cr = disk_cr.replace('.', '')
@@ -374,22 +377,22 @@ def windowsinfomaster(master_ip, master_name):
 
 
 # MAIN section
-windowsinfo("192.168.100.200","HYPNOS")
-windowsinfo("192.168.100.201","THANATOS")
-windowsinfo("192.168.100.202","ULTRAMAGNUS")
-windowsinfo("192.168.100.205","PHOBOS")
-windowsinfo("192.168.100.206","OPTIMUS")
+# windowsinfo("192.168.100.200","HYPNOS")
+# windowsinfo("192.168.100.201","THANATOS")
+# windowsinfo("192.168.100.202","ULTRAMAGNUS")
+# windowsinfo("192.168.100.205","PHOBOS")
+# windowsinfo("192.168.100.206","OPTIMUS")
 
-itwasdeleted("192.168.100.200","HYPNOS")
-itwasdeleted("192.168.100.201","THANATOS")
-itwasdeleted("192.168.100.202","ULTRAMAGNUS")
-itwasdeleted("192.168.100.205","PHOBOS")
-itwasdeleted("192.168.100.206","OPTIMUS")
+# itwasdeleted("192.168.100.200","HYPNOS")
+# itwasdeleted("192.168.100.201","THANATOS")
+# itwasdeleted("192.168.100.202","ULTRAMAGNUS")
+# itwasdeleted("192.168.100.205","PHOBOS")
+# itwasdeleted("192.168.100.206","OPTIMUS")
 
-windowsinfomaster("192.168.100.200","HYPNOS")
+# windowsinfomaster("192.168.100.200","HYPNOS")
 windowsinfomaster("192.168.100.201","THANATOS")
-windowsinfomaster("192.168.100.202","ULTRAMAGNUS")
-windowsinfomaster("192.168.100.205","PHOBOS")
-windowsinfomaster("192.168.100.206","OPTIMUS")
+# windowsinfomaster("192.168.100.202","ULTRAMAGNUS")
+# windowsinfomaster("192.168.100.205","PHOBOS")
+# windowsinfomaster("192.168.100.206","OPTIMUS")
 
 verifyipserver()
